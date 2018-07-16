8b5d39c4d92367dddd8491e663a2ff654b60da94
Pradeep <pradeep005@gmail.com>
2018-07-16 10:39:17 +0200

    Initial commit

---
+++ flashMgr.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <flashMgr.h>
+
+
+void flmMrTask()
+{
+  while(1)
+  {
+	  
+  }
+
+}
---
+++ tempMgr.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<tempMgr.h>
+
+int currTemp,prevTemp;
+
+
+void tempMgrTask()
+{
+   while(1)
+   {	
+      if(currTemp < prevTemp)
+      {
+
+      }
+   } 	   
+
+}
+
+
